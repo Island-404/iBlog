aff3c09e79458736c148ac583a934a59cebfc4ed
Island-404 <83696283+Island-404@users.noreply.github.com>
2021-06-08 20:08:26 +0800

    Update vue.config.js

--- vue-blog/blog/vue.config.js
+++ vue-blog/blog/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://47.106.155.215:8081",
+        target: "http://localhost:8081",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
